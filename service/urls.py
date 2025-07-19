657519f5872c4acc97cf2ce9719d313f1952ce11
mosfes <mosfes@gmail.com>
2025-07-19 23:33:35 +0700

    AAA

--- service/urls.py
+++ service/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     path('',include("repair.urls"))
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
